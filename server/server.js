a3b4dd51740c039f938f4eb231bd16b406da7cf0
Yunxin Liu <1999yunxin@gmail.com>
2024-08-12 15:59:46 -0700

    import dotenv

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
 app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+    console.log(`Server is running on port ${PORT}`);
 });
