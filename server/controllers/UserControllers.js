742d9fd88a738936f616aa50d6af3fd0365215d3
Yunxin Liu <1999yunxin@gmail.com>
2024-08-12 17:46:37 -0700

    debug user model and db connection, edit user signup controller

--- server/controllers/UserControllers.js
+++ server/controllers/UserControllers.js
@@ -1,21 +1,46 @@
+import User from '../models/UserModel.js';
+import bcrypyt from 'bcryptjs';
+
 // @desc: signup a new user
 // @route: POST /api/users/auth/signup
 // @access: Public
 export const signup = async (req, res) => {
-    const {username, email, password} = req.body;
-    const userExists = await User.findOne({email});
+    const {username, password, comfirmPassword, gender} = req.body;
+    // validate user input
+    if (comfirmPassword !== password) {
+        res.status(400);
+        throw new Error('Passwords do not match');
+    }
+    // check if user already exists
+    const userExists = await User.findOne({username});
     if(userExists){
         res.status(400);
         throw new Error('User already exists');
     }
+    // avatar
+    const maleProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+    const femaleProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+    // salt password
+    const salt = await bcrypyt.genSalt(10);
+    const hashedPassword = await bcrypyt.hash(password, salt);
+    // create user
     const user = await User.create({
         username,
-        email,
-        password
+        password: hashedPassword,
+        gender,
+        profilePic: gender == "male" ? maleProfilePic : femaleProfilePic,
+    });
+
+    await user.save();
+    // JWT token
+    res.status(201).json({
+        _id: user._id,
+        username: user.username,
+        profilePic: user.profilePic,
     });
 }
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
     res.send('login page');
 }
 
--- server/server.js
+++ server/server.js
@@ -11,17 +11,13 @@ import connectDB from './db/db.js';
 
 // MARK: create express app
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
-
 // MARK: middleware
+app.use(express.json()); // parse json data (req.body)
 app.use('/api/users', UserRoutes);
 
-// MARK: listen to port
+
 app.listen(5000, () => {
     connectDB();
     console.log(`Server is running on port ${PORT}`);
