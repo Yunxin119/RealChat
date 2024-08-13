9e0053b7d9fc43ce6544203c935a6977c485d1b8
Yunxin Liu <1999yunxin@gmail.com>
2024-08-12 17:53:04 -0700

    generate token and set cookie util

---
+++ server/utils/generateToken.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+const generateTokenAndCookie = (id, res) => {
+    const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+
+    res.cookie("jwt", token, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true, // this is to prevent XSS attacks
+        samesite: 'strict', // this is to prevent CSRF attacks
+    })
+    return token;
+}
