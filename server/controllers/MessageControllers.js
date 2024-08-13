56dfb5d6c6b75fbee3b5f012ad259729869e447e
Yunxin Liu <1999yunxin@gmail.com>
2024-08-13 12:09:54 -0700

    getMessages Controller

--- server/controllers/MessageControllers.js
+++ server/controllers/MessageControllers.js
@@ -28,12 +28,35 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id);
         }
 
+        // TODO: Socket.io implementation
+
+        // save conversation and message to database
         await conversation.save();
         await newMessage.save();
 
         res.status(201).json({message: "Message sent successfully"});
 
 
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).json({message: error.message});
+    }
+}
+
+export const getMessages = async (req, res) => {
+    try {
+        const receiver = req.params.id;
+        const sender = req.user._id; // get sender from authenticated user
+
+        let conversation = await Conversation.findOne({members: {$all: [sender, receiver]}}).populate("messages"); // populate messages in conversation
+        // populate here is a method that allows you to reference documents in other collections
+        // populate("messages") will populate messages in conversation
+
+        // if conversation does not exist, return error
+        if (!conversation) {
+            return res.status(404).json({message: "No messages found"});
+        }
+        res.status(200).json(conversation.messages);
     } catch (error) {
         console.error(error.message);
         res.status(400).json({message: error.message});
--- server/routes/MessageRoutes.js
+++ server/routes/MessageRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { sendMessage } from '../controllers/MessageControllers.js';
+import { sendMessage, getMessages } from '../controllers/MessageControllers.js';
 import { protectMiddleware } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 // MARK: Message Routes
-router.route('/send/:id').post(protectMiddleware, sendMessage);
+router.route('/send/:id').post(protectMiddleware, sendMessage)
+router.route('/:id').get(protectMiddleware, getMessages);
 
 export default router;
