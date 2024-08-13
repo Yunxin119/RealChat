06fd97de15bfc106d8dd7e8d756368ef6985863b
Yunxin Liu <1999yunxin@gmail.com>
2024-08-12 17:17:05 -0700

    user model using mongoose

---
+++ server/models/UserModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["male", "female"]
+    },
+    profilePic: {
+        type: String,
+        default: ""
+    },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
